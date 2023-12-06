05e9255dbeacacdfa43d51926895a13f591cf7c8
Bruce Li <150874228+Bruceli-iscool@users.noreply.github.com>
2023-12-06 10:36:30 -0800

    Update and rename main to main.js

--- John.com/main.js
+++ John.com/main.js
@@ -1,7 +1,7 @@
-<script>
 let answers = ''
 let x = answers
 console.log('welcome to Javascript learner.')
 console.log('I am your tutor for today.')
 console.log('Lets start!')
 console.log('The Basics: console.log function')
+// not part of the website
