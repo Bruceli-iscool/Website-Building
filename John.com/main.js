ed5e3d0276ed4132b5f3a818aa3a407a21697c79
Bruce Li <150874228+Bruceli-iscool@users.noreply.github.com>
2023-12-07 11:54:35 -0800

    Update main.js

--- John.com/main.js
+++ John.com/main.js
@@ -5,3 +5,4 @@ console.log('I am your tutor for today.')
 console.log('Lets start!')
 console.log('The Basics: console.log function')
 // not part of the website
+//you can probaly log it in as a prompt?
